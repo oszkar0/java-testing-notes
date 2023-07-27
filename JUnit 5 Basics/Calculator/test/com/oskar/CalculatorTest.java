7c47d6681f865e7a2afc09b99ccdd50343dab1e1
oszkar0 <oskszysiak@gmail.com>
2023-07-27 17:02:14 +0200

    Test Structure AAA

--- JUnit 5 Basics/Calculator/test/com/oskar/CalculatorTest.java
+++ JUnit 5 Basics/Calculator/test/com/oskar/CalculatorTest.java
@@ -12,23 +12,32 @@ class CalculatorTest {
     @DisplayName("Test 4/2=2")
     @org.junit.jupiter.api.Test
     void testDivision_WhenFourDividedByTwo_ShouldReturnTwo() {
+        //Arrange
         Calculator calculator = new Calculator();
+        int dividend = 4;
+        int divisor = 2;
+        int expectedResult = 2;
+
+        //Act
         int result = calculator.division(4, 2);
 
+        //Assert
         assertEquals(2 , result, "Test failed, 4/2 did not produce 2"); //<- message is optional, will be printed if test is failed
     }
 
     @DisplayName("Test 7-9=-2")
     @Test
     void testSubtraction_WhenNineIsSubtractedFromSeven_ShouldReturnMinusTwo() {
+        //Arrange //Given
         Calculator calculator = new Calculator();
-
         int minuend = 7;
         int subtrahend = 9;
         int expectedResult = -2;
 
+        //Act    //When
         int result = calculator.subtraction(minuend, subtrahend);
 
+        //Assert //Then
         assertEquals(expectedResult, result, ()->minuend + "-" + subtrahend + "did not produce " + expectedResult
                 + ", " + result + " was produced"); //<- it is good to put a message as a lambda, as it is computed only
                                                     //in case when test fails, if it is normal message it is always computed
