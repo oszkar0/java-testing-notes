3eb829d465c745f2a03a1be82e7991b02cd2a42a
oszkar0 <oskszysiak@gmail.com>
2023-07-27 16:29:41 +0200

    add subtraction method and its test

--- JUnit 5 Basics/Calculator/src/com/oskar/Calculator.java
+++ JUnit 5 Basics/Calculator/src/com/oskar/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int division(int dividend, int divisor){
         return dividend / divisor;
     }
+
+    public int subtraction(int minuend, int subtrahend){
+        return minuend - subtrahend;
+    }
 }
--- JUnit 5 Basics/Calculator/test/com/oskar/CalculatorTest.java
+++ JUnit 5 Basics/Calculator/test/com/oskar/CalculatorTest.java
@@ -1,5 +1,7 @@
 package com.oskar;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -10,5 +12,20 @@ class CalculatorTest {
         int result = calculator.division(4, 2);
 
         assertEquals(2 , result, "Test failed, 4/2 did not produce 2"); //<- message is optional, will be printed if test is failed
+    }
+
+    @Test
+    void subtraction() {
+        Calculator calculator = new Calculator();
+
+        int minuend = 7;
+        int subtrahend = 9;
+        int expectedResult = -2;
+
+        int result = calculator.subtraction(minuend, subtrahend);
+
+        assertEquals(expectedResult, result, ()->minuend + "-" + subtrahend + "did not produce " + expectedResult
+                + ", " + result + " was produced"); //<- it is good to put a message as a lambda, as it is computed only
+                                                    //in case when test fails, if it is normal message it is always computed
     }
 }
