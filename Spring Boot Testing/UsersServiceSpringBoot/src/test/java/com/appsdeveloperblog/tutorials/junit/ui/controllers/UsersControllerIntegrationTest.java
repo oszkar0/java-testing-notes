efb1984242054b96553d362e6fa42f075c8b2352
oszkar0 <oskszysiak@gmail.com>
2023-07-30 21:19:45 +0200

    add test for all layers: test get user when missing JWT

--- Spring Boot Testing/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ Spring Boot Testing/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -9,9 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.security.core.userdetails.User;
 
 import java.util.Arrays;
+import java.util.List;
 
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) Spring framework creates app context with mocked web environment
 //Only Web Layer beans will be created
@@ -54,5 +57,28 @@ public class UsersControllerIntegrationTest {
         Assertions.assertEquals(userDetailsRequestJson.getString("firstName"), createdUserDetails.getFirstName(),
                 "Returned user first name incorrect");
         Assertions.assertFalse(createdUserDetails.getUserId().trim().isEmpty(), "User id should not be empty");
+    }
+
+    @Test
+    @DisplayName("GET /users requires JWT")
+    void testGetUsers_whenMissingJWT_return405(){
+        //Arrange
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+
+        HttpEntity requestEntity = new HttpEntity(null, headers);
+
+        //Act
+        ResponseEntity<List<UserRest>> response = testRestTemplate.exchange("/users",
+                HttpMethod.GET,
+                requestEntity,
+                new ParameterizedTypeReference<List<UserRest>>() {
+                });
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode(), "Http Status code 403 " +
+                "Forbidden should have been returned");
+
+
     }
 }
